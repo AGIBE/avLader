041355c850cf6de216c03af3fc3fe524b9732090
Peter Schär <peter.schaer@bve.be.ch>
2016-01-08 08:00:55 +0100

    iLader-Usecase aus der Config auslesen.

--- avLader/helpers/release_helper.py
+++ avLader/helpers/release_helper.py
@@ -34,8 +34,9 @@ def release(gprcode):
         check_sql ="SELECT TASK_OBJECTID FROM TB_TASK WHERE GZS_OBJECTID=%s AND TASK_STATUS=2" % (gzs_objectid)
         check_result = avLader.helpers.sql_helper.readSQL(config['GEODB_DD_TEAM']['connection_string'], check_sql)
         sysdate = config['NACHFUEHRUNG'][gprcode.lower()]
+        usecase = unicode(config['ILADER_USESCASES'][gprcode.lower()])
         if len(check_result) == 0:
-            task_sql = "INSERT INTO tb_task (TASK_OBJECTID, GZS_OBJECTID, UC_OBJECTID, TASK_STATUS, FLAG_OBJECTID) VALUES (%s, %s, 6, 2, %s)" % (task_objectid, gzs_objectid, flag_objectid)
+            task_sql = "INSERT INTO tb_task (TASK_OBJECTID, GZS_OBJECTID, UC_OBJECTID, TASK_STATUS, FLAG_OBJECTID) VALUES (%s, %s, %s, 2, %s)" % (task_objectid, gzs_objectid, usecase, flag_objectid)
             logger.info("TB_TASK wird aktualisiert:")
             logger.info(task_sql)
             flag_sql = "INSERT INTO tb_flag_agi (FLAG_OBJECTID, GPR_BEZEICHNUNG, FLAG_DATUM, TASK_OBJECTID) VALUES (%s, '%s', %s, %s)" % (flag_objectid, gprcode, sysdate, task_objectid)
