9dca637ede0d6cc4570c4d6c94da17176fa56929
Peter Schär <peter.schaer@bve.be.ch>
2016-03-11 17:45:01 +0100

    mopube_import: QA eingefügt.

--- avLader/scripts/mopube_import.py
+++ avLader/scripts/mopube_import.py
@@ -4,10 +4,12 @@ import avLader.helpers.config_helper
 import avLader.helpers.connection_helper
 import avLader.helpers.ftp_helper
 import avLader.helpers.index_helper
+import fmeobjects
 import os
 import arcpy
 import sys
 import shutil
+import datetime
 
 def download_statistics(config, logger):
     statistics_filenames = config['ZAV_FTP']['statistics_files']
@@ -85,28 +87,28 @@ def run():
     ]
     
     download_statistics(config, logger)
-    
-    source_fgdb = avLader.helpers.ftp_helper.download_fgdb(config['ZAV_FTP']['mopube_filename'], config, logger)
      
+    source_fgdb = avLader.helpers.ftp_helper.download_fgdb(config['ZAV_FTP']['mopube_filename'], config, logger)
+      
     downloaded_zip = os.path.join(config['DIRECTORIES']['local_data_dir'], config['ZAV_FTP']['mopube_filename'])
     export_zip = os.path.join(config['DIRECTORIES']['zips'], "MOPUBE.zip")
     logger.info("Export-ZIP wird kopiert nach " + export_zip)
     shutil.copy2(downloaded_zip, export_zip)
-     
+      
     target_sde = config['NORM_TEAM']['connection_file']
-     
+      
     for mopube_object in mopube_objects:
         logger.info("Importiere " + mopube_object)
         source_object = os.path.join(source_fgdb, mopube_object)
         target_object = os.path.join(target_sde, config['NORM_TEAM']['username'] + "." + mopube_object)
-          
+           
         needs_spatial_index = False
         if arcpy.Describe(target_object).datasetType == "FeatureClass":
             needs_spatial_index = True
-          
+           
         if arcpy.Exists(source_object):
             if arcpy.Exists(target_object):
-                  
+                   
                 # Räumlichen Index entfernen
                 if needs_spatial_index and arcpy.Describe(target_object).hasSpatialIndex:
                     logger.info("Spatial Index wird entfernt.")
@@ -115,13 +117,13 @@ def run():
                         logger.info("Spatial Index erfolgreich entfernt.")
                     else:
                         logger.warn("Spatial Index konnte wegen eines Locks nicht entfernt werden.")
-                  
+                   
                 logger.info("Truncating " + target_object)
                 arcpy.TruncateTable_management(target_object)
-                  
+                   
                 logger.info("Appending " + source_object)
                 arcpy.Append_management(source_object, target_object, "TEST")
-                  
+                   
                 # Räumlichen Index erstellen
                 if needs_spatial_index:
                     logger.info("Spatial Index wird erstellt.")
@@ -136,13 +138,13 @@ def run():
                         logger.info("Spatial Index erfolgreich erstellt.")
                     else:
                         logger.warn("Spatial Index konnte wegen eines Locks nicht erstellt werden.")
-                  
+                   
                 logger.info("Zähle Records in der Quelle und im Ziel.")
                 source_count = int(arcpy.GetCount_management(source_object)[0])
                 logger.info("Anzahl Records in der Quelle: " + unicode(source_count))
                 target_count = int(arcpy.GetCount_management(target_object)[0])
                 logger.info("Anzahl Records im Ziel: " + unicode(target_count))
-                  
+                   
                 if source_count==target_count:
                     logger.info("Anzahl Records identisch")
                 else:
@@ -160,5 +162,34 @@ def run():
             logger.error("Import wird abgebrochen.")
             avLader.helpers.connection_helper.delete_connection_files(config, logger)
             sys.exit()
+    
+    # QA-Script
+    fme_script_qa = os.path.splitext(__file__)[0] + "_qa.fmw"
+    fme_logfile_qa = avLader.helpers.fme_helper.prepare_fme_log(fme_script_qa, config['LOGGING']['log_directory'])
+    
+    qa_filename = "mopube_import_qa" + datetime.datetime.now().strftime("_%Y_%m_%d_%H_%M_%S") + ".xls"
+    qa_file = os.path.join(config['LOGGING']['log_directory'], qa_filename)
+    logger.info("Das QA-Excelfile lautet: " + qa_file)
+    
+    runner = fmeobjects.FMEWorkspaceRunner()
+    # Der FMEWorkspaceRunner akzeptiert keine Unicode-Strings!
+    # Daher müssen workspace und parameters umgewandelt werden!
+    parameters = {
+        'NORM_DATABASE': str(config['NORM_TEAM']['database']),
+        'NORM_USERNAME': str(config['NORM_TEAM']['username']),
+        'NORM_PASSWORD': str(config['NORM_TEAM']['password']),
+        'VEK1_DATABASE': str(config['GEO_VEK1']['database']),
+        'VEK1_USERNAME': str(config['GEO_VEK1']['username']),
+        'VEK1_PASSWORD': str(config['GEO_VEK1']['password']),
+        'QA_EXCEL': str(qa_file),
+        'LOGFILE': str(fme_logfile_qa)
+    }
+    try:
+        runner.runWithParameters(str(fme_script_qa), parameters)
+    except fmeobjects.FMEException as ex:
+        logger.error("FME-Workbench " + fme_script_qa + " konnte nicht ausgeführt werden!")
+        logger.error(ex)
+        logger.error("Import wird abgebrochen!")
+        sys.exit()    
     
     avLader.helpers.connection_helper.delete_connection_files(config, logger)
