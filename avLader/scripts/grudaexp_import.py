cf046e60ba51667ce7923764b8841f5c979fb5d0
Martina Köhli <martina.koehli@bve.be.ch>
2016-10-19 12:25:18 +0200

    VV2: Checksript GRUDAEXP eingebaut

--- avLader/scripts/grudaexp_import.py
+++ avLader/scripts/grudaexp_import.py
@@ -6,6 +6,7 @@ import avLader.helpers.fme_helper
 import fmeobjects
 import os
 import sys
+import datetime
 
 def run():
     config = avLader.helpers.config_helper.get_config('grudaexp_import')
@@ -14,12 +15,20 @@ def run():
     
     fme_script = os.path.splitext(__file__)[0] + ".fmw"
     fme_logfile = avLader.helpers.fme_helper.prepare_fme_log(fme_script, config['LOGGING']['log_directory'])
-
+    
+    fme_script_qa = os.path.splitext(__file__)[0] + "_qa.fmw"
+    fme_logfile_qa = avLader.helpers.fme_helper.prepare_fme_log(fme_script_qa, config['LOGGING']['log_directory'])
+    
+    qa_filename = "grudaexp_import_qa" + datetime.datetime.now().strftime("_%Y_%m_%d_%H_%M_%S") + ".xls"
+    qa_file = os.path.join(config['LOGGING']['log_directory'], qa_filename)
+    logger.info("Das QA-Excelfile lautet: " + qa_file)
+    
     logger.info("Script " +  fme_script + " wird ausgeführt.")
     logger.info("Das FME-Logfile heisst: " + fme_logfile)
     
     itf_file = os.path.join(config['DIRECTORIES']['gruda'], config['GRUDA_FTP']['filename'])
     
+    # Import-Script
     runner = fmeobjects.FMEWorkspaceRunner()
     # Der FMEWorkspaceRunner akzeptiert keine Unicode-Strings!
     # Daher müssen workspace und parameters umgewandelt werden!
@@ -38,6 +47,31 @@ def run():
         logger.error(ex)
         logger.error("Import wird abgebrochen!")
         sys.exit()    
+    
+    logger.info("Script " +  fme_script_qa + " wird ausgeführt.")
+    logger.info("Das FME-Logfile heisst: " + fme_logfile_qa)
+    
+    # QA-Script
+    runner = fmeobjects.FMEWorkspaceRunner()
+    # Der FMEWorkspaceRunner akzeptiert keine Unicode-Strings!
+    # Daher müssen workspace und parameters umgewandelt werden!
+    parameters = {
+        'NORM_DATABASE': str(config['NORM_TEAM']['database']),
+        'NORM_USERNAME': str(config['NORM_TEAM']['username']),
+        'NORM_PASSWORD': str(config['NORM_TEAM']['password']),
+        'VEK1_DATABASE': str(config['GEO_VEK1']['database']),
+        'VEK1_USERNAME': str(config['GEO_VEK1']['username']),
+        'VEK1_PASSWORD': str(config['GEO_VEK1']['password']),
+        'QA_EXCEL': str(qa_file),
+        'LOGFILE': str(fme_logfile_qa)
+    }
+    try:
+        runner.runWithParameters(str(fme_script_qa), parameters)
+    except fmeobjects.FMEException as ex:
+        logger.error("FME-Workbench " + fme_script + " konnte nicht ausgeführt werden!")
+        logger.error(ex)
+        logger.error("QA-FME wird abgebrochen!")
+        sys.exit()
     
     avLader.helpers.connection_helper.delete_connection_files(config, logger)
 
