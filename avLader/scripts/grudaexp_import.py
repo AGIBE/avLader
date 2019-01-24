0d65070cdccf3fa6705b7dcf830dfecbd86064d4
Peter Schaer <peter.schaer@bve.be.ch>
2019-01-24 16:51:56 +0100

    #1343 GRUDAEXP-Import eingebaut.

--- avLader/scripts/grudaexp_import.py
+++ avLader/scripts/grudaexp_import.py
@@ -9,12 +9,66 @@ import sys
 import datetime
 import shutil
 import glob
+import zipfile
+
+def getSuffix(filename):
+    filename_only = os.path.basename(filename)
+    without_extension = os.path.splitext(filename_only)[0]
+    suffix = without_extension.split("gruda_export")[1]
+    return suffix
+
+def renameCSVFile(prefix, dirname, suffix):
+    old_name = os.path.join(dirname, prefix + suffix + ".csv")
+    new_name = os.path.join(dirname, prefix + ".csv")
+    os.rename(old_name, new_name)
+
 
 def run():
     config = avLader.helpers.config_helper.get_config('grudaexp_import')
     logger = config['LOGGING']['logger']
     logger.info("GRUDAEXP-Import wird ausgeführt.")
     
+    # GRUDA-Export herunterladen und vorbereiten
+    logger.info("Daten werden vorbereitet.")
+    list_of_files = glob.glob(config['DIRECTORIES']['gruda_lieferung']) 
+    latest_file = max(list_of_files, key=os.path.getctime)
+
+    # Die CSV-Files heissen bei jedem Export anders.
+    # Sie haben immer ein Zeit- und Datumsbasiertes Suffix
+    # Es wird ermittelt, damit die CSV-Filenamen manipuliert
+    # werden können.
+    suffix = getSuffix(latest_file)
+    logger.info("Suffix: " + suffix)
+    
+    zip_file_ori = os.path.join(config['DIRECTORIES']['gruda_lieferung'], latest_file)
+    zip_filename = config['DIRECTORIES']['gruda_filename']
+
+    zip_file = os.path.join(config['DIRECTORIES']['local_data_dir'], zip_filename)
+    
+    # copy and replace old zip-file
+    shutil.copyfile(zip_file_ori, zip_file)
+    logger.info("Datei wird kopiert von " + zip_file_ori + " nach " + zip_file)
+
+    # Lösche allfällige CSV-Files in local_data_dir
+    filestodelete = glob.glob(os.path.join(config['DIRECTORIES']['local_data_dir'], "*.csv"))
+    logger.info("Lösche CSV-Files in " + config['DIRECTORIES']['local_data_dir'])
+    for f in filestodelete:
+        logger.info(f)
+        os.remove(os.path.join(config['DIRECTORIES']['local_data_dir'], f))
+    
+    # Zipfile entpacken
+    with zipfile.ZipFile(zip_file) as grudazip:
+        logger.info("Entpacke Zipfile " + zip_file)
+        grudazip.extractall(config['DIRECTORIES']['local_data_dir'])
+
+    # CSV-Files umbenennen
+    csv_filenames = ["gebaeude", "grundstueck_gebaeude", "gebaeude_eingang_adresse", "grundstueck", "bodenbedeckung_anteil"]
+    for csv_filename in csv_filenames:
+        logger.info("Benenne CSV-File um: " + csv_filename)
+        renameCSVFile(csv_filename, config['DIRECTORIES']['local_data_dir'], suffix)
+
+    csv_folder_fme = os.path.join(config['DIRECTORIES']['local_data_dir'], "*.csv")
+
     fme_script = os.path.splitext(__file__)[0] + ".fmw"
     fme_logfile = avLader.helpers.fme_helper.prepare_fme_log(fme_script, config['LOGGING']['log_directory'])
     
@@ -28,18 +82,6 @@ def run():
     logger.info("Script " +  fme_script + " wird ausgeführt.")
     logger.info("Das FME-Logfile heisst: " + fme_logfile)
 
-    # GRUDA-Export herunterladen und vorbereiten
-    list_of_files = glob.glob(config['DIRECTORIES']['gruda_lieferung']) 
-    latest_file = max(list_of_files, key=os.path.getctime)
-    
-    zip_file_ori = os.path.join(config['DIRECTORIES']['gruda_lieferung'], latest_file)
-    zip_filename = config['DIRECTORIES']['gruda_filename']
-    zip_file = os.path.join(config['DIRECTORIES']['gruda'], zip_filename)
-    
-    # copy and replace old zip-file
-    shutil.copyfile(zip_file_ori, zip_file)
-    logger.info("Datei wird kopiert von " + zip_file_ori + " nach " + zip_file)
-    
     # Import-Script
     runner = fmeobjects.FMEWorkspaceRunner()
     # Der FMEWorkspaceRunner akzeptiert keine Unicode-Strings!
@@ -48,8 +90,7 @@ def run():
         'DATABASE': str(config['NORM_TEAM']['database']),
         'USERNAME': str(config['NORM_TEAM']['username']),
         'PASSWORD': str(config['NORM_TEAM']['password']),
-        'MODELLABLAGE': str(config['DIRECTORIES']['models']),
-        'ITF_FILE': str(zip_file),
+        'CSVFOLDER': str(csv_folder_fme),
         'LOGFILE': str(fme_logfile)
     }
     try:
