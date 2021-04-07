b99f1faf8d3160f75a472d24412a82d8f8420b6a
Peter Schär <peter.schaer@be.ch>
2021-04-07 08:57:19 +0200

    Performance-Verbesserungen

--- src/avLader/scripts/lk_import.py
+++ src/avLader/scripts/lk_import.py
@@ -7,6 +7,8 @@ import AGILib.downloader
 import os
 import sys
 import zipfile 
+import tempfile
+import shutil
 
 def unzip_zip(zip_file, config, logger):
     logger.info("Entpacke Zip-File.")
@@ -20,10 +22,18 @@ def run():
     logger.info("%s wird ausgeführt." % (subcommand))
     
     # Download des GPKG
-    logger.info("GPKG-Download für LK wird ausgeführt.")
-    ftp = AGILib.downloader.FTPDownloader(dest_dir=config['LK_PARAMETER']['lk_gpkg'], ftp_host=config['LK_FTP']['host'], ftp_username=config['LK_FTP']['username'], ftp_password=config['LK_FTP']['password'], ftp_directory=config['LK_FTP']['directory'], ftp_filenames=config['LK_FTP']['lk_filename'])
+    gpkg_tempdir = tempfile.mkdtemp()
+    logger.info("GPKG wird in temporäres Directory heruntergeladen.")
+    gpkg_tempfile = os.path.join(gpkg_tempdir, config['LK_FTP']['lk_filename'])
+    logger.info(gpkg_tempdir)
+    ftp = AGILib.downloader.FTPDownloader(dest_dir=gpkg_tempdir, ftp_host=config['LK_FTP']['host'], ftp_username=config['LK_FTP']['username'], ftp_password=config['LK_FTP']['password'], ftp_directory=config['LK_FTP']['directory'], ftp_filenames=config['LK_FTP']['lk_filename'])
     ftp.download()
-    unzip_zip(os.path.join(config['LK_PARAMETER']['lk_gpkg'], config['LK_FTP']['lk_filename']), config, logger)
+    # shutil.copy(r"\\geoda.infra.be.ch\arbeit\Rampe\LKMAP\gpkg\LKMap_be.zip", gpkg_tempdir)
+    logger.info("GPKG wird entzippt.")
+    with zipfile.ZipFile(os.path.join(gpkg_tempdir, config['LK_FTP']['lk_filename'])) as lkzip:
+        lkzip.extractall(gpkg_tempdir)
+    logger.info("GPKG wird auf die Rampe kopiert.")
+    shutil.copy2(gpkg_tempfile, config['LK_PARAMETER']['lk_gpkg'])
     
     # FME-Skripte
     # FME 1: LKMETA_gpkg2Norm: Schreibt die LKMETA-Daten vom Geopackage aufs NORM
@@ -33,7 +43,7 @@ def run():
     parameters = {
         'SourceDataset_XLSXR_5': config['LK_PARAMETER']['lk_meta_wt'],
         'SourceDataset_XLSXR_4': config['LK_PARAMETER']['lk_etapp'],
-        'LKBE_gpkg': os.path.join(config['LK_PARAMETER']['lk_gpkg'], config['LK_PARAMETER']['lk_gpkg_file']),
+        'LKBE_gpkg': gpkg_tempfile,
         'DestDataset_GEODATABASE_FILE': config['LK_PARAMETER']['lk_gdb_stand'],
         'DestDataset_XLSXW': config['LK_PARAMETER']['lk_xlsx_stand'],
         'SourceDataset_XLSXR': config['LK_PARAMETER']['lk_xlsx_stand'],
@@ -61,7 +71,7 @@ def run():
 
     parameters2 = {
         'SourceDataset_XLSXR': config['LK_PARAMETER']['lk_map_wt'],
-        'LKBE_gpkg': os.path.join(config['LK_PARAMETER']['lk_gpkg'], config['LK_PARAMETER']['lk_gpkg_file']),
+        'LKBE_gpkg': gpkg_tempfile,
         'POSTGIS_DB': config['NORM_TEAM_PG']['database'],
         'POSTGIS_HOST': config['NORM_TEAM_PG']['host'],
         'POSTGIS_PORT': unicode(config['NORM_TEAM_PG']['port']),
@@ -85,4 +95,5 @@ def run():
     gp = 'LKMAP'
     avLader.scripts.lk_import_qa.check_count_features(config, logger, gp, quelle_pg = True, gemeinde = True)
     
+    shutil.rmtree(gpkg_tempdir)
     avLader.helpers.helper.delete_connection_files(config, logger)
